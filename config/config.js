080722bb818cf8f7cdb36557e2f533ef159ac44e
Ronald Lima <ronaldazevedolima@gmail.com>
2021-02-05 15:34:29 -0300

    testando banco

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.SCHEMA,
-    host: process.env.HOSTNAME,
+    username: process.env.HEROKU_USER || process.env.MYSQL_USER,
+    password: process.env.HEROKU_PASSWORD ||process.env.MYSQL_PASSWORD,
+    database: process.env.HEROKU_DB ||process.env.SCHEMA,
+    host: process.env.HEROKU_HOSTNAME ||process.env.HOSTNAME,
     dialect: 'mysql',
     logging: false,
   },
